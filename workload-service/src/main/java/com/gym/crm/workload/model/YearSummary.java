e4adbe890d40a9d814ab1da9a47cf08f3a43524b
Artem Kraynyk <kraynyk_a2015@ukr.net>
2026-06-16 09:31:30 +0300

    GIA-247 Implement Workload microservice - add models

---
+++ workload-service/src/main/java/com/gym/crm/workload/model/YearSummary.java
@@ -0,0 +1,14 @@
+package com.gym.crm.workload.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class YearSummary {
+
+    private Integer year;
+    private List<MonthSummary> months;
+}
