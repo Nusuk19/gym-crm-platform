4a4d7160314fe541e611dd8208e672529fdd2cd9
Artem Krainyk <kraynyk_a2015@ukr.net>
2026-06-20 11:59:13 +0300

    GIA-251 Implement circuit breaker
    * GIA-251 Implement circuit breaker
    
    * GIA-251 Implement сircuit breaker - edit application-test.yml

--- gym-core-service/src/main/java/com/gym/crm/core/client/workload/WorkloadServiceClient.java
+++ gym-core-service/src/main/java/com/gym/crm/core/client/workload/WorkloadServiceClient.java
@@ -1,21 +1,32 @@
 package com.gym.crm.core.client.workload;
 
 import com.gym.crm.core.client.workload.model.TrainerWorkloadRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class WorkloadServiceClient {
 
+    private static final String WORKLOAD_CB = "workload-service";
+
     private final RestClient workloadRestClient;
 
+    @CircuitBreaker(name = WORKLOAD_CB, fallbackMethod = "fallback")
     public void updateTrainerWorkload(TrainerWorkloadRequest request) {
         workloadRestClient.put()
                 .uri("/trainer-workloads")
                 .body(request)
                 .retrieve()
                 .toBodilessEntity();
+    }
+
+    private void fallback(TrainerWorkloadRequest request, Exception ex) {
+        log.error("Circuit breaker triggered for workload update: trainer={}, error={}",
+                request.getTrainerUsername(), ex.getMessage());
     }
 }
