5862eabf2e809b6a3ff4bfcef40be022fbcf5c40
Artem Kraynyk <kraynyk_a2015@ukr.net>
2026-06-23 21:07:47 +0300

    GIA-274 Implement error handling for timeout responses - removed unused imports

--- gym-core-service/src/test/java/com/gym/crm/core/client/workload/WorkloadServiceClientCircuitBreakerTest.java
+++ gym-core-service/src/test/java/com/gym/crm/core/client/workload/WorkloadServiceClientCircuitBreakerTest.java
@@ -14,7 +14,6 @@ import java.time.LocalDate;
 import java.time.Month;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
