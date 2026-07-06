d047631da1d9621d283a0649d785acec875b5755
Artem Kraynyk <kraynyk_a2015@ukr.net>
2026-07-06 14:49:22 +0300

    GIA-317 Create trainer workload MongoDB schema

--- workload-service/src/main/java/com/gym/crm/workload/model/TrainerWorkload.java
+++ workload-service/src/main/java/com/gym/crm/workload/model/TrainerWorkload.java
@@ -1,19 +1,43 @@
 package com.gym.crm.workload.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.List;
 
+import static org.springframework.data.mongodb.core.mapping.Field.Write.NON_NULL;
+
 @Getter
 @Setter
+@Builder
+@Document(collection = "trainer_workloads")
+@NoArgsConstructor
 @AllArgsConstructor
+@CompoundIndex(
+        name = "trainer_name_idx",
+        def = "{'trainerFirstName': 1, 'trainerLastName': 1}")
 public class TrainerWorkload {
 
+    @Id
+    @Field(name = "_id", write = NON_NULL)
     private String trainerUsername;
+
+    @Field(name = "trainerFirstName", write = NON_NULL)
     private String trainerFirstName;
+
+    @Field(name = "trainerLastName", write = NON_NULL)
     private String trainerLastName;
+
+    @Field(name = "isActive", write = NON_NULL)
     private Boolean isActive;
+
+    @Field(name = "years", write = NON_NULL)
     private List<YearSummary> years;
 }
