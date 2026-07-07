41cb38c909256718d80bf892ec62c24bbb3f0ab5
Artem Krainyk <kraynyk_a2015@ukr.net>
2026-07-07 11:06:49 +0300

    GIA-317 Create trainer workload MongoDB schema
    GIA-317 Create trainer workload MongoDB schema

--- workload-service/src/main/java/com/gym/crm/workload/model/MonthSummary.java
+++ workload-service/src/main/java/com/gym/crm/workload/model/MonthSummary.java
@@ -1,14 +1,24 @@
 package com.gym.crm.workload.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import static org.springframework.data.mongodb.core.mapping.Field.Write.NON_NULL;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class MonthSummary {
 
+    @Field(name = "month", write = NON_NULL)
     private Integer month;
+
+    @Field(name = "trainingSummaryDuration", write = NON_NULL)
     private Integer trainingSummaryDuration;
 }
--- workload-service/src/main/java/com/gym/crm/workload/model/YearSummary.java
+++ workload-service/src/main/java/com/gym/crm/workload/model/YearSummary.java
@@ -1,14 +1,26 @@
 package com.gym.crm.workload.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.List;
 
+import static org.springframework.data.mongodb.core.mapping.Field.Write.NON_NULL;
+
 @Getter
+@Setter
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class YearSummary {
 
+    @Field(name = "year", write = NON_NULL)
     private Integer year;
+
+    @Field(name = "months", write = NON_NULL)
     private List<MonthSummary> months;
 }
