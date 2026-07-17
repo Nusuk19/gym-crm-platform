5281d82f9abf7a7375e073cc5ecd1e2c70903b86
Artem Kraynyk <kraynyk_a2015@ukr.net>
2026-07-17 21:05:52 +0300

    GIA-340 Implement workload-service component tests - extract helper method for try catch

--- gym-automation-tests/src/test/java/com/gym/crm/automation/client/WorkloadQueuePublisher.java
+++ gym-automation-tests/src/test/java/com/gym/crm/automation/client/WorkloadQueuePublisher.java
@@ -23,17 +23,20 @@ public class WorkloadQueuePublisher {
 
         try (Connection connection = connectionFactory.createConnection(TestProperties.brokerUser(), TestProperties.brokerPassword())) {
             connection.start();
-
-            try (Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)) {
-                Queue queue = session.createQueue(QUEUE_NAME);
-                try (MessageProducer producer = session.createProducer(queue)) {
-                    var message = session.createTextMessage(toJson(workloadMessage));
-                    message.setStringProperty("_type", MESSAGE_TYPE);
-                    producer.send(message);
-                }
-            }
+            sendMessage(connection, workloadMessage);
         } catch (JMSException e) {
             throw new IllegalStateException(String.format("Failed to publish workload message to %s", QUEUE_NAME), e);
+        }
+    }
+
+    private void sendMessage(Connection connection, Map<String, Object> workloadMessage) throws JMSException {
+        try (Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)) {
+            Queue queue = session.createQueue(QUEUE_NAME);
+            try (MessageProducer producer = session.createProducer(queue)) {
+                var message = session.createTextMessage(toJson(workloadMessage));
+                message.setStringProperty("_type", MESSAGE_TYPE);
+                producer.send(message);
+            }
         }
     }
 
